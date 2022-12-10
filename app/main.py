d005a9555d0af75bc2a3c255ca9858b4f070df2c
User <yusuf@DESKTOP-K4GQLQG>
2022-12-10 16:10:38 +0530

    Second Stage

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    client_connection, client_address = server_socket.accept() # wait for client
+    client_connection.recv(4096)
+    client_connection.send(bytes('+PONG\r\n', 'UTF-8'))
+    
 
 if __name__ == "__main__":
     main()
